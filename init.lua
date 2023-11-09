f9ebdb31fb71545644cd77f7f862f5464336d7e5
RAkasheh <rakasheh@eastnets.com>
2023-11-09 10:22:24 +0300

    🚸: QOL updates - vim surround - Theme catppuccin/nvim - lock.json

--- init.lua
+++ init.lua
@@ -76,6 +76,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- Extra added plugins 
+  'tpope/vim-surround',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -152,12 +155,71 @@ require('lazy').setup({
     },
   },
 
-  {
+  --[[ {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
+    end,
+  }, ]]
+
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+          flavour = "mocha", -- latte, frappe, macchiato, mocha
+          background = { -- :h background
+              light = "latte",
+              dark = "mocha",
+          },
+          transparent_background = false, -- disables setting the background color.
+          show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+          term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+          dim_inactive = {
+              enabled = true, -- dims the background color of inactive window
+              shade = "dark",
+              percentage = 0.8, -- percentage of the shade to apply to the inactive window
+          },
+          no_italic = false, -- Force no italic
+          no_bold = false, -- Force no bold
+          no_underline = false, -- Force no underline
+          styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+              comments = { "italic" }, -- Change the style of comments
+              conditionals = { "italic" },
+              loops = {},
+              functions = {},
+              keywords = {},
+              strings = {},
+              variables = {},
+              numbers = {},
+              booleans = {},
+              properties = {},
+              types = {},
+              operators = {},
+          },
+          color_overrides = {
+              mocha = {
+                base = "#000000",
+              }
+          },
+          custom_highlights = {},
+          integrations = {
+              cmp = true,
+              gitsigns = true,
+              nvimtree = true,
+              treesitter = true,
+              notify = false,
+              mini = {
+                  enabled = true,
+                  indentscope_color = "",
+              },
+              -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+          },
+      })
+
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -168,7 +230,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
