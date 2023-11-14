5bcd1d058f1a4db9c8f90f1eba93abf4c7ae61f8
RAkasheh <rakasheh@eastnets.com>
2023-11-14 14:46:18 +0300

    🐛: fixed extra characters from copilot accept

--- init.lua
+++ init.lua
@@ -332,8 +332,8 @@ require('lazy').setup({
     config = function()
       vim.keymap.set({ 'n', 'o', 'x' }, '<leader>cp', ':Copilot panel<cr>',
         { desc = '[C]opilot [P]anel', silent = true, noremap = true })
-      vim.keymap.set('i', '<M-j>', [[copilot#Accept("\<CR>")]],
-        { silent = true, script = true, expr = true })
+      vim.keymap.set("i", "<M-j>", "copilot#Accept('<CR>')",
+        { noremap = true, silent = true, expr = true, replace_keycodes = false })
     end,
   },
 
