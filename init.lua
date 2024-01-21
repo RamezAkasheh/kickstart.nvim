30656b304a0a0de69b5fc02a7731327d771ac424
RAkasheh <rakasheh@eastnets.com>
2024-01-21 12:41:15 +0300

    :children_crossing: fugitive register desc

--- init.lua
+++ init.lua
@@ -171,47 +171,32 @@ require('lazy').setup({
         -- visual mode
         map('v', '<leader>hs', function()
           gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
+        end, { desc = 'git [H]unk [S]tage' })
         map('v', '<leader>hr', function()
           gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
+        end, { desc = 'git [H]unk [R]eset' })
         -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git [H]unk [S]tage' })
+        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git [H]unk [R]eset' })
+        -- map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
+        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'git [H]unk [U]ndo stage' })
+        -- map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = 'git [H]unk [P]review' })
         map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+          gs.blame_line { full = true }
+        end, { desc = 'git [H]unk [B]lame' })
+        map('n', '<leader>hd', gs.diffthis, { desc = 'git [H]unk [D]iff against index' })
         map('n', '<leader>hD', function()
           gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
+        end, { desc = 'git [H]unk [D]iff last commit' })
+        map('n', '<leader>ga', ':Git commit --amend --no-edit<cr>', { desc = '[G]it [A]mend' })
 
         -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'git [T]oggle [B]lame line' })
+        map('n', '<leader>td', gs.toggle_deleted, { desc = 'git [T]oggle [D]eleted' })
 
         -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-
---         -- Actions
---         vim.keymap.set({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<cr>', { desc = 'git [H]unk [S]tage selected' })
---         vim.keymap.set({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<cr>', { desc = 'git [H]unk [R]eset selected' })
---         vim.keymap.set('n', '<leader>ha', gs.stage_hunk, { desc = 'git [H]unk [A]add' })
---         vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'git [H]unk [U]ndo stage' })
---         -- vim.keymap.set('n', '<leader>hR', gs.reset_buffer , {desc='git [H]unk [R]eset BUFFER'})
---         vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { buffer = bufnr, desc = 'git [H]unk [P]review' })
---         vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'git [H]unk [B]lame' })
---         vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'git [H]unk [D]iff' })
---         vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'git [H]unk [D]iff last commit' })
---         vim.keymap.set('n', '<leader>td', gs.toggle_deleted, { desc = 'git [T]oggle [D]eleted' })
---         vim.keymap.set('n', '<leader>ga', ':Git commit --amend --no-edit<cr>', { desc = 'git [A]mend' })
---
---         -- text object
---         vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[I]nside [H]unk' })
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[I]nside [H]unk' })
       end,
     },
   },
