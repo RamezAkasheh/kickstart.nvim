4002ed43937a8a398aaa88030d0446f48834bf9d
RAkasheh <rakasheh@eastnets.com>
2024-07-11 11:33:11 +0300

    :children_crossing: fixed wrap remap for visual mode

--- init.lua
+++ init.lua
@@ -225,8 +225,8 @@ vim.keymap.set({ 'n', 'v' }, 'x', '"xx', { silent = true })
 vim.keymap.set({ 'n', 'v' }, 'X', '"xX', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.opt.wrap = false
 
 -- productivity keymaps
