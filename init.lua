28153af10428c49dce76cf67cfca9fb119f1b3c2
RAkasheh <rakasheh@eastnets.com>
2024-07-13 14:52:27 +0300

    :alien: update

--- init.lua
+++ init.lua
@@ -359,12 +359,17 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[c]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[d]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>r', group = '[r]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[s]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>w', group = '[w]orkspace' },
+        { '<leader>w_', hidden = true },
       }
     end,
   },
